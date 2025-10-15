5866321cfbfadc81115b4eb86713062abd9541aa
maxim-malik <maxim.malik@stu.ocsb.ca>
2025-10-15 13:03:45 -0400

    added find distance using sonar

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Max
+ * Created on: Oct 2025
+ * This program shows distance using an ultrasonic sensor
 */
 
-basic.showString('Hello, World!')
+let distanceToObject:number = 0
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+//find distance with sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
